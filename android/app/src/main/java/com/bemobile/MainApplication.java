95967204d709139ae0ea5349ab69d92c613fd157
panda.cn <65720230@qq.com>
2018-09-03 18:24:28 +0800

    9-3

--- android/app/src/main/java/com/bemobile/MainApplication.java
+++ android/app/src/main/java/com/bemobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.bemobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.psykar.cookiemanager.CookieManagerPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new CookieManagerPackage(),
             new OrientationPackage(),
             new VectorIconsPackage()
